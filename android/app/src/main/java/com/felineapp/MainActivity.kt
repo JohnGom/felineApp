fae65d12022b8ecc52c5897bcc6b88fb3024573d
John Gomez <zemog1916@gmail.com>
2025-06-11 00:02:27 -0500

    fix android run

--- android/app/src/main/java/com/felineapp/MainActivity.kt
+++ android/app/src/main/java/com/felineapp/MainActivity.kt
@@ -5,7 +5,7 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle;
-Import org.devio.rn.splashscreen.Splashscreen;
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
 
